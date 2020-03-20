4db85e7c13320f0f7495edd0924e53af74a49e5f
Jonas Siewertsen <mail@jonassiewertsen.com>
2020-03-20 16:44:58 +0100

    cleaning up the service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,35 +6,16 @@ use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
-    protected $routes = [
-//        'cp' => __DIR__ . '/../routes/cp.php',
-    ];
-
-    protected $widgets = [
-//        \Jonassiewertsen\OhDear\Widgets\OhDear::class,
-    ];
-
     public function boot()
     {
         parent::boot();
 
         $this->bootMiddleware();
-
-//        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'oh-dear');
-//        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'oh-dear');
-//        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'oh-dear');
-//
-//        if ($this->app->runningInConsole()) {
-//            $this->publishes([
-//                __DIR__.'/../resources/lang' => resource_path('lang/vendor/jonassiewertsen/ohDear/'),
-//            ], 'Statamic Oh Dear lang file');
-//
-//            $this->publishes([
-//                __DIR__ . '/../config/config.php' => config_path('statamic/oh-dear.php'),
-//            ], 'Statamic Oh Dear config file');
-//        }
     }
 
+    /**
+     * Will add the cookie consent partial automatically to all views
+     */
     protected function bootMiddleware()
     {
         $router = $this->app['router'];
